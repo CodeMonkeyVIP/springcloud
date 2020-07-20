fdec289a3e18f0936a4e162f94ebc9efcfe6e167
CodeMonkeyVIP <codemonkeyvip@163.com>
2020-07-20 18:01:50 +0800

    Hystrix study

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Description :
+ * Author      : Guojingbin
+ * Create      : 2020.7.20 17:46
+ * Version     : 1.0
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
